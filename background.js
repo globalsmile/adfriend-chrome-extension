39255e94fd9565d59da98d43684f253e31a1f4f5
Mohammed Badmus <mohammedbadmus823@gmail.com>
2025-02-15 18:58:21 +0100

    Update background.js

--- background.js
+++ background.js
@@ -14,4 +14,27 @@ chrome.runtime.onInstalled.addListener(() => {
     console.log('Received message in background:', message);
     sendResponse({ status: 'received' });
   });
-  
+
+
+/*chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'getZenQuotes') {
+    fetch("https://zenquotes.io/api/quotes")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log("Fetched quotes in background:", data);
+        sendResponse({ success: true, quotes: data });
+      })
+      .catch(err => {
+        console.error("Error fetching ZenQuotes in background:", err);
+        sendResponse({ success: false, error: err.message });
+      });
+    // Indicate that we will send a response asynchronously.
+    return true;
+  }
+});
+*/
